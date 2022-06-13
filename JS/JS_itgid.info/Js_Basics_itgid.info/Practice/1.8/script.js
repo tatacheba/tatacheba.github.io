2099b55497ca966672a3c2838befe34f74b0c315
TatianaCheb <tanya.cheba12@gmail.com>
2022-06-13 12:29:48 +0500

    delete file

--- JS/JS_itgid.info/Js_Basics_itgid.info/Practice/1.8/script.js
+++
@@ -1,7 +0,0 @@
-let inputRange = document.querySelector('input[type=range]');
-let btn = document.querySelector('.button')
-
-inputRange.oninput = () => {
-    btn.style.background = '#' + inputRange.value;
-    console.log(btn.style.background);
-}
