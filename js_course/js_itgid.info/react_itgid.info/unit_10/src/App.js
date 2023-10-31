e00f43a8dc36b69bd4682d3562c16b96f2569b70
Tata.Chebotar <57943361+tatacheba@users.noreply.github.com>
2023-10-31 14:07:50 +0100

    upEnd unit_10

--- js_course/js_itgid.info/react_itgid.info/unit_10/src/App.js
+++ js_course/js_itgid.info/react_itgid.info/unit_10/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import In from "./In";
+import Out from "./Out";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <In />
+            <Out />
+        </div>
+    );
 }
 
 export default App;
